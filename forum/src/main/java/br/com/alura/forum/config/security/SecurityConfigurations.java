5492abeebab36d3034fa90c87f48e4a2e2c4369e
951549924@adm.unifor.br <gabrielcunha2012@gmail.com>
2022-11-29 16:38:31 -0300

    tratando bibliotecas depreciadas

--- forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -2,32 +2,59 @@ package br.com.alura.forum.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.http.HttpMethod;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.ldap.EmbeddedLdapServerContextSourceFactoryBean;
-import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations {
 
+    //Configuração de autenticação
     @Bean
-    public EmbeddedLdapServerContextSourceFactoryBean contextSourceFactoryBean() {
-        EmbeddedLdapServerContextSourceFactoryBean contextSourceFactoryBean =
-                EmbeddedLdapServerContextSourceFactoryBean.fromEmbeddedLdapServer();
-        contextSourceFactoryBean.setPort(0);
-        return contextSourceFactoryBean;
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .build();
+    }
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//    UserDetails user = User.withDefaultPasswordEncoder()
+//            .username("user")
+//            .password("password")
+//            .roles("USER")
+//            .build();
+//    auth.jdbcAuthentication()
+//            .withDefaultSchema()
+//            .dataSource(dataSource())
+//            .withUser(user);
+//}
+
+
+    //Configuração de autorização
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/topicos").permitAll()
+                .antMatchers(HttpMethod.GET, "/topicos/**").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin();
+        return http.build();
     }
 
-    @Bean
-    AuthenticationManager ldapAuthenticationManager(
-            BaseLdapPathContextSource contextSource) {
-        LdapBindAuthenticationManagerFactory factory =
-                new LdapBindAuthenticationManagerFactory(contextSource);
-        factory.setUserDnPatterns("uid={0},ou=people");
-        factory.setUserDetailsContextMapper(new PersonContextMapper());
-        return factory.createAuthenticationManager();
-    }
-
-
+    //Configuração de recursos estáticos (js, css, imagens)
+//    @Bean
+//    protected WebSecurityCustomizer webSecurityCustomizer(){
+//        return (web) -> web.ignoring().antMatchers("/ignore1", "/ignore2");
+//    }
 }
---
+++ forum/src/main/java/br/com/alura/forum/modelo/Perfil.java
@@ -0,0 +1,26 @@
+package br.com.alura.forum.modelo;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Perfil implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Getter @Setter
+    private Long id;
+    @Getter @Setter
+    private String nome;
+
+    @Override
+    public String getAuthority() {
+        return this.nome;
+    }
+}
